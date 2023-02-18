935369920fa6f0217d07ca48c057e7925a787204
adam040992 <adam040992@gmail.com>
2023-02-18 18:35:25 +0100

    Add: Data

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { BorwserRouter } from 'react-router-dom';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BorwserRouter>
+      <App />
+    </BorwserRouter>
   </React.StrictMode>
 );
